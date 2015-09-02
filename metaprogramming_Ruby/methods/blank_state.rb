388461b6e292099e201c06fa1b174ffdcd176d88
Diego Hung <yooyhung@gmail.com>
2015-09-02 22:53:05 +0700

    add notes for blank_state

---
+++ metaprogramming_Ruby/methods/blank_state.rb
@@ -0,0 +1,5 @@
+# when we want to have one object that has as few behavior as possible
+# we can inherit from BasicObject
+# BasicObject contains only the following methods:
+# [:==, :equal?, :!, :!=, :instance_eval, :instance_exec, :__send__, :__id__]
+
